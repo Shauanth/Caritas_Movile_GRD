400922056313c71c50e90e41a157368aac8c21b4
FABRIZZIO <fabrizzio.gomez@acj.pe>
2026-05-23 15:48:16 -0500

    Proyecto Inicializado

---
+++ app/src/main/java/pucp/edu/caritas_movile_grd/MainActivity.kt
@@ -0,0 +1,47 @@
+package pucp.edu.caritas_movile_grd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pucp.edu.caritas_movile_grd.ui.theme.Caritas_Movile_GRDTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Caritas_Movile_GRDTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Caritas_Movile_GRDTheme {
+        Greeting("Android")
+    }
+}
